f131433dbc215ccf9ee06893f925be0065ee261a
Inhan <auroram4nia@gmail.com>
2025-09-30 10:13:06 +0900

    add calculator001.cpp

--- note/calculator001.cpp
+++ note/calculator001.cpp
@@ -1,8 +1,10 @@
+
 import std;
 using namespace std;
 
-
+/************************************************************/
 /*							Error							*/
+/************************************************************/
 void error(const string& msg)
 {
 	throw runtime_error(msg);
@@ -25,11 +27,14 @@ public:
 /************************************************************/
 
 
+
+
 /************************************************************/
 /*						Token_Stream						*/
 /************************************************************/
 class Token_Stream{
 public:
+	Token_Stream() : full(false), buffer(0){}
 	Token get();
 	void putback(Token t);
 private:
@@ -76,36 +81,102 @@ Token Token_Stream::get()
 		}
 	default:
 		error("Bad Tokens");
-	}
-	
-	
+	}	
 }
 
 /************************************************************/
 /************************************************************/
-Token_Stream ts;
-
+Token_Stream ts;			// 아래의 함수에서 사용해야하니 선안
+double Expressions();		// 아래 Primary에서 사용해야하니 선언
 
 
 double Primary()
 {
-	double left = Term();
 	Token t = ts.get();
-
+	switch (t.kind) {
+	case'(':
+	{
+		double d = Expressions();
+		t = ts.get();
+		if (t.kind != ')') error("')' expected ");
+		return d;
+	}
+	case '8':
+		return t.value;
+	default:
+		error("primary expected");
+	}
 }
+
 double Term()
 {
-	double left = Term();
+	double left = Primary();
 	Token t = ts.get();
+	while (true) {
+		switch (t.kind) {
+		case '*':
+			left *= Primary();
+			t = ts.get();
+			break;
+		case '/':
+		{
+			double d = Primary();
+			if (d == 0) error("Divide by Zero");	// 0으로 나눌때 error
 
+			left /= d;
+			t = ts.get();
+			break;
+		}
+		//case '%':
+		default:
+			return left;
+		}
+	}
 }
+
 double Expressions()
 {
 	double left = Term();
 	Token t = ts.get();
-
-
-
+	while (true) {
+		switch (t.kind) {
+		case'+':
+			left += Term();
+			t = ts.get();
+			break;
+		case'-':
+			left -= Term();
+			t = ts.get();
+			break;
+		default:
+			return left;
+		}
+	}
 }
 
+
+
+
 /************************************************************/
+/*							main							*/					
+/************************************************************/
+int main(void)
+try {
+	while (cin) {
+		double val = 0;
+		cout << "> ";
+		Token t = ts.get();
+		if (t.kind == 'q' || t.kind == 'Q') break;
+		if (t.kind == ';') cout << "=" << val << '\n';
+		else ts.putback(t);
+		val = Expressions();
+	}
+}
+catch (exception& e) {
+	cerr << e.what() << '\n';
+	return 1;
+}
+catch (...) {
+	cerr << "exception" << '\n';
+	return 2;
+}
