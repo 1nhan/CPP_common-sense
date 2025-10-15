c850c68ac857b0f7918b8f1e3b226f3c86db6006
Inhan <auroram4nia@gmail.com>
2025-10-15 17:55:29 +0900

    add e8

--- Exercises/7. Technicalities Functions, etc/E07_09.cpp
+++ Exercises/7. Technicalities Functions, etc/E07_09.cpp
@@ -14,7 +14,8 @@ void error(const string& msg) {
 	throw runtime_error(msg);
 }
 
-vector<double>& PriceandWeight(const vector<double>& price, const vector<double>& weight) {
+vector<double> Mul_Price_And_Weight
+(const vector<double>& price, const vector<double>& weight) {
 	if (price.size() != weight.size()) { 
 		error("different size"); return ; 
 	}
@@ -24,5 +25,23 @@ vector<double>& PriceandWeight(const vector<double>& price, const vector<double>
 			index.push_back(price[i] * weight[i]);
 		}
 		return index;
+	}
+}
+
+int main() {
+
+	int how_size = -1;
+	cin >> how_size;
+	if (how_size < 1)error("none size");
+	vector<double>price(how_size);
+	vector<double>weight(how_size);
+
+	for (int i = 0; i < how_size; ++i) {
+		cin >> price[i] >> weight[i];
+	}
+
+	Mul_Price_And_Weight(price, weight);
+	for (int i = 0; i < how_size; ++i) {
+
 	}
 }
