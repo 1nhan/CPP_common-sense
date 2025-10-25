70ce06d8536a4bd3e9e3aa6aa0c3b2d39a64e4c4
Inhan <auroram4nia@gmail.com>
2025-10-25 16:57:14 +0900

    2025-1025

--- Exercises/4. Errors!/E4_12_13.cpp
+++ Exercises/4. Errors!/E4_12_13.cpp
@@ -6,7 +6,101 @@
 	사용자는 반복적으로 추측하여 그 숫자를 맞춰야 한다.예를 들어, 
 	정답이 1234이고 사용자가 1359를 입력하면, 출력은 “1 bull and 1 cow”가 되어야 한다.
 	추측은 네 개의 숫자를 정확한 순서로 맞출 때까지 계속된다.
+*/
+import std;
 
+using namespace std;
+
+auto error(const string& msg) -> void
+{
+	throw runtime_error(msg);
+}
+
+constexpr int rule = 4;
+
+auto prompt() -> void {
+	cout << "“Bulls and Cows”" << '\n';
+	cout << "you can only input 0-9" << '\n';
+}
+
+auto make_number_for_computer() -> vector<int>{
+	vector<int>four_number;
+	// ..four_number를 채우기
+	default_random_engine engine;
+	uniform_int_distribution dist(0,9);
+
+	while (four_number.size() < rule) {// 사이즈가 룰에 충족할때까지 반복
+		int num = dist(engine);
+		bool unique = true;// 중복 불가 플레그
+		for (int i = 0; i < four_number.size(); ++i) {
+			if (four_number[i] == num) {
+				unique = false;
+				break;
+			}
+		}
+		if (unique)//중복 아닐시 push_back
+			four_number.push_back(num);
+	}
+
+	/* 출력(확인용)
+	for (int i = 0; i < four_number.size(); ++i) {
+		cout << four_number[i] << ' ';
+	}
+	cout << '\n';
+	*/
+	return four_number;
+}
+
+auto make_number_for_user() -> vector<int>{
+	vector<int>four_number;
+	// ..four_number를 채우기
+	int numb = 0;
+	bool unique = true;
+	while (four_number.size() < rule) {
+		cin >> numb;
+		for(int i = 0; i< four_number.size();++i)
+			if (four_number[i] == numb) {
+				unique = false;
+				cout << "중복된 숫자입니다." << '\n';
+				break;
+			}
+		if (unique)
+			four_number.push_back(numb);
+	}
+	return four_number;
+}
+
+auto bull_cow() -> void
+{// 숫자가 존재하고 위치가 다르면 cow,
+ // 숫자가 존재하고 위치도 같으면 bull
+	vector<int>computer = make_number_for_computer();
+	vector<int>user = make_number_for_user();
+	int cow = 0, bull = 0;
+	prompt();
+	while (bull < rule)
+	{// bull이 4이면 반복은 종료
+		cow = 0;
+		bull = 0;
+		for (int i = 0; i < computer.size(); ++i) {
+			if (computer[i] == user[i])
+				++bull;
+		}//is bull?
+		for (int i = 0; i < computer.size(); ++i) {
+			for (int j = 0; j <= i; ++j)
+				if ((i != j) && (computer[i] == user[j]))
+					++cow;
+		}
+		cout << "“" << bull << " bull and " << cow << " cow”" << '\n';
+		prompt();
+		user = make_number_for_user();
+	}
+}
+
+auto main() -> int
+{
+	bull_cow();
+}
+/*
 	13
 	위 게임은 정답이 하드코딩되어 있어 반복 플레이가 어렵다. 
 	프로그램을 수정하여 사용자가 반복적으로 플레이할 수 있도록 하고, 
@@ -17,95 +111,3 @@
 	이 n은 시드(seed)라 불리며, 서로 다른 시드는 서로 다른 난수 시퀀스를 생성한다.
 * 
 *************************************************************************/
-
-#include<algorithm>
-import std;
-using namespace std;
-
-/* 게임의 룰 4개의 숫자만을 rule_is_four로 정의 */
-constexpr int rule_is_four = 4;	
-
-/*벡터 size에 대한 유효성 검사*/
-bool Is_Size_Four(vector<int> computer_para_input, vector<int> user_para_input)
-{
-	if (computer_para_input.size() > 4 || user_para_input.size() > 4)
-	{
-		cout << "인덱스 범위를 초과했습니다. 프로그램을 재시작해주세요." << '\n';
-		return false;
-	}
-	return true;
-}
-
-/*Bulls 함수*/
-int Bulls(vector<int> computer_para_input, vector<int> user_para_input)
-{
-	int bull = 0;
-	Is_Size_Four(computer_para_input, user_para_input);
-	for (int x = 0; x < rule_is_four; ++x)
-		if (computer_para_input[x] == user_para_input[x]) ++bull;
-	return bull;
-}
-/*Cows 함수*/
-int Cows(vector<int> computer_para_input, vector<int> user_para_input)
-{
-	int cow = 0;
-	Is_Size_Four(computer_para_input, user_para_input);
-	if (computer_para_input.size() > 4 || user_para_input.size() > 4) return 0;
-	for (int x = 0; x < rule_is_four; ++x)
-		for (int y = 0; y < rule_is_four; ++y)
-			if (computer_para_input[x] != user_para_input[x] &&
-				computer_para_input[x] == user_para_input[y]) ++cow;
-	return cow;
-}
-/*Bulls and Cows 함수 끝*/
-
-int main(void)
-{
-	cout << "“Bulls and Cows”" << '\n';
-
-	/*균등분포를 이용한 컴퓨터벡터 vector 선언 */
-	default_random_engine engine;				//기본 난수 생성기 engine객체
-	uniform_int_distribution<int> dist(0, 9);	//0-9까지 균등하게 분포되게 반환
-	vector<int>computer_vector;					//변수 computer_input 선언
-
-	/* 유저벡터 vector 선언 */
-	int user_input = -1;
-	vector<int>user_vector;
-	
-	while(computer_vector.size()<=4)				//백터에 기본난수생성기를 이용해 (0-9까지)균등하게 담기.
-	{	
-		computer_vector.push_back(dist(engine));
-	}
-
-	for (int x = 0; x < rule_is_four; ++x)
-	{
-		cout << computer_vector[x];
-	}
-	cout << '\n';
-	/* user_input입력후 벡터 user_vector에 저장*/
-	cout << "네개의 수를 입력하세요" << '\n';
-	while(cin>> user_input)
-	{
-		user_vector.push_back(user_input);
-		/*user_vector의 크기가 4일때부터 비교*/
-		if (user_vector.size() == 4)
-		{
-			if (Bulls(computer_vector, user_vector) != 4)
-			{
-				cout << "“"		<< Bulls(computer_vector, user_vector)
-				<< "bull and "	<< Cows(computer_vector, user_vector)
-				<< "cow”" << '\n';
-				user_vector.clear();
-			}
-			else { cout <<"4Bulls. GAME OVER" << '\n'; return 0; }
-		}
-	}
-}
-
-/* 1차시도 실패.
-
-1. 중첩 반복문 속 y가 증가하지 않고 x가 증가하게끔 이상한 반복문을 만듬.(논리 오류) 
-2. user_input이 push_back하니 한번만 반복되고 두번째부터는 아마도 user_input 5,6,7,8로 
-증가되어 반복문이 내 생각처럼 반복이 안됨. (논리 오류)
-
-*/
