5ff6179f91f13ae8853a1b11de53345b0aaa8297
Inhan <auroram4nia@gmai.com>
2025-12-29 18:16:16 +0900

    Update DSA_Linked List

---
+++ DSA/08_Linked_List/Linked_List_01_Display.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Node
+{
+	int data; 
+	struct Node* next;
+}*first = NULL; //
+
+
+void Create(int A[], int n)
+{
+	// 새 노드 생성에 도움을 주는 t와 마지막 노드를 가리키는 last 
+	struct Node		*t,	 
+					*last; 
+	
+	first			= (struct Node*)malloc(sizeof(struct Node));
+	first->data		= A[0];
+	first->next		= NULL;
+	last			= first;
+
+	for (int i = 1; i < n; ++i)
+	{
+		t			= (struct Node*)malloc(sizeof(struct Node));
+		t->data		= A[i];
+		t->next		= NULL;
+		last->next	= t;
+		last		= t;
+	}
+}
+
+void Display(struct Node* p)
+{
+	while (p)
+	{
+		printf("%d ", p->data);
+		p = p->next;
+	}
+	printf("\n");
+}
+
+int main()
+{
+	int A[] = { 3,5,7,9,13 };
+	Create(A, 5);
+	Display(first);
+	return 0;
+}
--- DSA/08_Linked_List/README.md
+++ DSA/08_Linked_List/README.md
@@ -6,4 +6,16 @@
 ### Problem with Array
 
 ### Difference Between Array and Linked List
-  
+
+###
+```
+first ──► [data=A[0] | next=NULL]
+last  ──┘
+
+first ──► [data=A[0] | next= ──► [data=A[1] | next=NULL]]
+last ───────────────────────────────┘
+
+first ──► [A[0]] → [A[1]] → [A[2]] → ... → [A[n]]
+last ───────────────────────────────────────────┘
+
+```
